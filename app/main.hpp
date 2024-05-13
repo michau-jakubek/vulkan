4eacc88fc86564b5123686cc46aa12cd30765f3e
michau-jakubek <michau.jakubek@gmail.com>
2024-05-14 01:12:29 +0200

    Add third-party libraries

--- app/main.hpp
+++ app/main.hpp
@@ -3,10 +3,11 @@
 
 #include "vtfVkUtils.hpp"
 
-constexpr vtf::Version CurrentVtfVersion(1, 1, 2, 0);
+constexpr vtf::Version CurrentVtfVersion(1, 1, 3, 0);
 
 // 2024-04-10, 1.1.0.0, "Add versioning"
 // 2024-04-15, 1.1.1.0, "Some fixes in int_compute test."
 // 2024-04-19, 1.1.2.0, "Add params to fractals test"
+// 2024-05-14, 1.1.3.0, "Add third-party libraries"
 
 #endif // __MAIN_HPP_INCLUDED__
